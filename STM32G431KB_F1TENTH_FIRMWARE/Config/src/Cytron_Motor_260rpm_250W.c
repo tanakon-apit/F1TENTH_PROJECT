f08166d69d9b614a61dc8f8019c775aa8b7bb4fd
tanakon-apit <78376947+pukkuu@users.noreply.github.com>
2024-03-20 16:27:54 +0700

    Project Setup

---
+++ STM32G431KB_F1TENTH_FIRMWARE/Config/src/Cytron_Motor_260rpm_250W.c
@@ -0,0 +1,12 @@
+/*
+ * Cytron_Motor_260rpm_250W.c
+ *
+ *  Created on: Jan 27, 2024
+ *      Author: 08809
+ */
+
+#include "Cytron_Motor_260rpm_250W.h"
+
+/* USER CODE BEGIN */
+
+/* USER CODE END */
